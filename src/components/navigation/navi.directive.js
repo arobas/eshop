edcfa81b28d1c6d0b7925bdad21ae1c4d326962b
arobas <robas.a@gmail.com>
2016-05-08 16:27:29 +0200

    Added files via upload

---
+++ src/components/navigation/navi.directive.js
@@ -0,0 +1,12 @@
+angular.module('app').directive('initialMenu', function(){
+
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/navi.directive.html',
+        controller: function($scope){
+
+            
+            }
+
+        }
+});
